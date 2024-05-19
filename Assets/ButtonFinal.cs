981b718b7ebe4a0331392769ee014ecf3300f483
PranavDivichenchu <146238990+PranavDivichenchu@users.noreply.github.com>
2024-05-19 08:23:18 -0400

    endgamekeycode
    pew

---
+++ Assets/ButtonFinal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ButtonFinal : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI myTextelement;
+    string code = "896";
+    public void toggleOn(int num)
+    {
+        myTextelement.text += num + "";
+        if (myTextelement.text.Length > code.Length)
+        {
+            myTextelement.text = "";
+        } 
+
+        finishGame();
+    }
+
+    public void finishGame()
+    {
+        if (myTextelement.text.Equals(code))
+        {
+            return;
+        }
+    }
+}
